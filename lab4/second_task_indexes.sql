a9f0d3797836940eec753db4dd82305cda9034b5
razgonyaevm <razgonyaevm@gmail.com>
2025-05-17 09:38:43 +0300

    Added files for the second request

---
+++ lab4/second_task_indexes.sql
@@ -0,0 +1,8 @@
+CREATE INDEX idx_н_люди_фамилия ON "Н_ЛЮДИ" USING btree ("ФАМИЛИЯ");
+CREATE INDEX idx_н_обучения_члвк_ид ON "Н_ОБУЧЕНИЯ" USING btree ("ЧЛВК_ИД");
+CREATE INDEX idx_н_ученики_начало ON "Н_УЧЕНИКИ" USING btree ("НАЧАЛО");
+CREATE INDEX idx_н_ученики_члвк_ид ON "Н_УЧЕНИКИ" USING btree ("ЧЛВК_ИД");
+CREATE INDEX idx_н_ученики_начало ON "Н_УЧЕНИКИ" USING btree ("НАЧАЛО");
+
+
+
